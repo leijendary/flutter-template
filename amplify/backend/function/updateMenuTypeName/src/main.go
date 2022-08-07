3dedcb54f944b73b259de811f4c9735bf8265b02
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2022-08-07 21:50:53 +0200

    used golang for lambda

---
+++ amplify/backend/function/updateMenuTypeName/src/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+type ItemInfo struct {
+	Id string `json:"id"`
+}
+
+type TypeNameEvent struct {
+	TypeName string `json:"typeName"`
+}
+
+func HandleRequest(ctx context.Context, typeName TypeNameEvent) (map[string]int, error) {
+	region := os.Getenv("REGION")
+	tableName := os.Getenv("API_SAMPLE_MENUTABLE_NAME")
+	cfg, _ := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	svc := dynamodb.NewFromConfig(cfg)
+	out, _ := svc.Scan(context.TODO(), &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	})
+	count := 0
+
+	for _, item := range out.Items {
+		itemInfo := ItemInfo{}
+		_ = attributevalue.UnmarshalMap(item, &itemInfo)
+		input := &dynamodb.UpdateItemInput{
+			TableName: aws.String(tableName),
+			Key: map[string]types.AttributeValue{
+				"id": &types.AttributeValueMemberS{
+					Value: itemInfo.Id,
+				},
+			},
+			ExpressionAttributeValues: map[string]types.AttributeValue{
+				":tn": &types.AttributeValueMemberS{Value: typeName.TypeName},
+			},
+			ReturnValues:     types.ReturnValueUpdatedNew,
+			UpdateExpression: aws.String("SET typeName = :tn REMOVE type"),
+		}
+
+		_, err := svc.UpdateItem(context.TODO(), input)
+
+		if err != nil {
+			log.Fatalf("Got error calling UpdateItem: %s", err)
+		} else {
+			count++
+		}
+	}
+
+	return map[string]int{"count": count}, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
